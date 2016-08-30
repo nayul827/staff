07e783f98bc88b1aeef8b19ced7811dcb4079fe3
김나율 <thdud0827@naver.com>
2016-08-30 17:47:05 +0900

    sasdf

--- project/src/com/project/project/dao/ProjectDao.java
+++ project/src/com/project/project/dao/ProjectDao.java
@@ -1,5 +1,60 @@
 package com.project.project.dao;
 
-public class ProjectDao {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.project.common.DBManager;
+import com.project.project.dto.ProjectDto;
+
+public class ProjectDao {
+	private ProjectDao(){
+		
+	}
+	private static ProjectDao instance = new ProjectDao();
+	
+	public static ProjectDao getInstance(){
+		if(instance == null) {
+			instance = new ProjectDao();
+		}
+		return instance;
+	}
+	public List<ProjectDto> selectAllMember(){
+		String sql = "select * from emp";
+		List<ProjectDto> list = new ArrayList<>();
+		
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DBManager.getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			while (rs.next()) {
+				ProjectDto pDto = new ProjectDto();
+				
+				pDto.setPpronum(rs.getInt("ppronum"));
+				pDto.setEmpno(rs.getString("empno"));
+				pDto.setPpronm(rs.getString("ppronm"));
+				pDto.setWdt(rs.getString("wdt"));
+				
+				list.add(pDto);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DBManager.close(conn, stmt, rs);
+		}
+		return list;
+	}
+	public void insertProject(ProjectDto pDto) {
+		String sql = "insert into pproject("
+				+ "PPRONUM,PREGIDT,PINITDT,POUTDT,PPRONM,PCONTENT,EMPNO,WORKNUM,CAREERNUM)"
+				+ "values(ppronum,?,?,?,?,?,?,?,?)";
+	}
+	
 }
--- project/src/com/project/project/dto/ProjectDto.java
+++ project/src/com/project/project/dto/ProjectDto.java
@@ -1,54 +1,139 @@
 package com.project.project.dto;
 
 public class ProjectDto {
-//	 cpronum	 NUMBER(6)
-//     ,	 cregidate	 CHAR(8)
-//     ,	 cinitdate	 CHAR(8)
-//     ,	 coutdate	 CHAR(8)
-//     ,	 cproname	 VARCHAR2(30)
-//     ,	 ccontent	 VARCHAR2(2500)
+//	CPRONUM	NUMBER(6)
+//	CREGIDT	CHAR(8)
+//	CINITDT	CHAR(8)
+//	COUTDT	CHAR(8)
+//	CPRONM	VARCHAR2(30)
+//	CCONTENT	VARCHAR2(2500)
+	
 	private int cpronum;
-	private String cregidate;
-	private String cinitdate;
-	private String coutdate;
-	private String cproname;
-	private String ccontet;
+	private String cregidt;
+	private String cinitdt;
+	private String coutdt;
+	private String cpronm;
+	private String ccontent;
+	
 	public int getCpronum() {
 		return cpronum;
 	}
 	public void setCpronum(int cpronum) {
 		this.cpronum = cpronum;
 	}
-	public String getCregidate() {
-		return cregidate;
+	public String getCregidt() {
+		return cregidt;
 	}
-	public void setCregidate(String cregidate) {
-		this.cregidate = cregidate;
+	public void setCregidt(String cregidt) {
+		this.cregidt = cregidt;
 	}
-	public String getCinitdate() {
-		return cinitdate;
+	public String getCinitdt() {
+		return cinitdt;
 	}
-	public void setCinitdate(String cinitdate) {
-		this.cinitdate = cinitdate;
+	public void setCinitdt(String cinitdt) {
+		this.cinitdt = cinitdt;
 	}
-	public String getCoutdate() {
-		return coutdate;
+	public String getCoutdt() {
+		return coutdt;
 	}
-	public void setCoutdate(String coutdate) {
-		this.coutdate = coutdate;
+	public void setCoutdt(String coutdt) {
+		this.coutdt = coutdt;
 	}
-	public String getCproname() {
-		return cproname;
+	public String getCpronm() {
+		return cpronm;
 	}
-	public void setCproname(String cproname) {
-		this.cproname = cproname;
+	public void setCpronm(String cpronm) {
+		this.cpronm = cpronm;
 	}
-	public String getCcontet() {
-		return ccontet;
+	public String getCcontent() {
+		return ccontent;
 	}
-	public void setCcontet(String ccontet) {
-		this.ccontet = ccontet;
+	public void setCcontent(String ccontent) {
+		this.ccontent = ccontent;
 	}
 	
+//	
+//	
+//	PPRONUM	NUMBER(5)
+//	PREGIDT	CHAR(8)
+//	PINITDT	CHAR(8)
+//	POUTDT	CHAR(8)
+//	PPRONM	VARCHAR2(20)
+//	PCONTENT	VARCHAR2(2500)
+//	EMPNO	CHAR(10)
+//	WORKNUM	NUMBER(2)
+//	CAREERNUM	NUMBER(3)
+	
+	private int ppronum;
+	private String pregidt;
+	private String pinitdt;
+	private String poutdt;
+	private String ppronm;
+	private String pcontent;
+	private String empno;
+	private int worknum;
+	private int careernum;
+	private String wdt;
+
+	public String getWdt() {
+		return wdt;
+	}
+	public void setWdt(String wdt) {
+		this.wdt = wdt;
+	}
+	public int getPpronum() {
+		return ppronum;
+	}
+	public void setPpronum(int ppronum) {
+		this.ppronum = ppronum;
+	}
+	public String getPregidt() {
+		return pregidt;
+	}
+	public void setPregidt(String pregidt) {
+		this.pregidt = pregidt;
+	}
+	public String getPinitdt() {
+		return pinitdt;
+	}
+	public void setPinitdt(String pinitdt) {
+		this.pinitdt = pinitdt;
+	}
+	public String getPoutdt() {
+		return poutdt;
+	}
+	public void setPoutdt(String poutdt) {
+		this.poutdt = poutdt;
+	}
+	public String getPpronm() {
+		return ppronm;
+	}
+	public void setPpronm(String ppronm) {
+		this.ppronm = ppronm;
+	}
+	public String getPcontent() {
+		return pcontent;
+	}
+	public void setPcontent(String pcontent) {
+		this.pcontent = pcontent;
+	}
+	public String getEmpno() {
+		return empno;
+	}
+	public void setEmpno(String empno) {
+		this.empno = empno;
+	}
+	public int getWorknum() {
+		return worknum;
+	}
+	public void setWorknum(int worknum) {
+		this.worknum = worknum;
+	}
+	public int getCareernum() {
+		return careernum;
+	}
+	public void setCareernum(int careernum) {
+		this.careernum = careernum;
+	}
 	
 }
